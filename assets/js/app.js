a235daeb2159450ecd08af385f0cad527cb60adf
JasonKurtzWebDeveloper <JasonKurtzWebDeveloper@gmail.com>
2018-04-08 19:18:32 -0400

    added click event functions and listners. added preloader for home page on initial load. updated the links in the menu and logo to call page content from our WordPress API.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+// inside out project STEP-105
+
 window.onload = init();
 
 function init() {
@@ -16,52 +18,61 @@ function init() {
 
 	$.ajax({
 		method: 'GET',
-		url: 'https://me.studingwebdev.com/wp-json/wp-api-menus/v2/menus/2',
+		url: '//me.studingwebdev.com/wp-json/wp-api-menus/v2/menus/2',
 		dataType: 'json',
 		success: function (data) {
-
 			$('nav').hide();
-			
 			var menu = menuBuilder(data.items);
-
 			$('nav').html(menu).slideDown();
-			
+			$('nav li a').click(function () {
+				getPage($(this).data("pgid"));
+			});
+			getPage(47);
 			$("#loaderDiv").fadeOut("slow");
-
 		},
 		error: function () {
 			console.log('all is not good');
 		}
 	});
-
 }
 
 function menuBuilder(obj) {
-
 	var theMenu = '';
-
 	if (obj.length > 0) {
-
 		theMenu = theMenu + '<ul>';
-
 		obj.forEach(function (item) {
-
-			theMenu = theMenu + '<li><a href="#">' + item.title + '</a>';
-
+			theMenu = theMenu + '<li><a href="#" data-pgid="' + item.object_id + '">' + item.title + '</a>';
 			if (item.children) {
 				theMenu = theMenu + menuBuilder(item.children);
 			}
-
 			theMenu = theMenu + '</li>';
-
 		});
-
 		theMenu = theMenu + '</ul>';
-
 	} else {
 		console.log('no data');
 	}
-
 	return theMenu;
+}
 
+function getPage(obj) {
+	$("#loaderDiv").fadeIn("slow");
+	$.ajax({
+		method: 'GET',
+		url: '//me.studingwebdev.com/wp-json/wp/v2/pages/' + obj,
+		dataType: 'json',
+		success: function (data) {
+			var pgbuild = '';
+			pgbuild = '<section><div class="container">' + data.content.rendered + '</div></section>';
+			$("#content").fadeOut(function () {
+				$('html').animate({
+					scrollTop: 0
+				}, 'slow'); //IE, FF
+				$('body').animate({
+					scrollTop: 0
+				}, 'slow'); //Chrome, not sure if Safari works
+				$(this).html(pgbuild).fadeIn();
+				$("#loaderDiv").fadeOut("slow");
+			});
+		}
+	});
 }
